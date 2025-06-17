3782331dfd8a56a89f45876ba361f7d3b32433b7
cmartins13 <cmartins@adcecija.pt>
2025-06-17 12:15:12 +0100

    Revert "Add"
    This reverts commit 4938d4733e8080ccab63f4fc06affaba5f9ba7d3.

--- app/teste/src/components/Footer.vue
+++ app/teste/src/components/Footer.vue
@@ -12,7 +12,7 @@
         <div class="footer-info">
           <div class="developer-info">
             <h3>Developer</h3>
-            <p class="developer-name">Claudio Martins & Joaquim Hilling</p>
+            <p class="developer-name">Claudio Martins</p>
           </div>
           <div class="contact-info">
             <h3>Contact</h3>
